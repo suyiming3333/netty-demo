956c5c7dc35f187438f0dd861b808473729e3ae7
suyiming <suyiming3333@gmail.com>
2019-12-22 17:24:19 +0800

    netty heartbeat

--- src/main/java/com/sym/netty/client/SimpleClientHandler.java
+++ src/main/java/com/sym/netty/client/SimpleClientHandler.java
@@ -7,4 +7,16 @@ public class SimpleClientHandler extends SimpleChannelInboundHandler<String> {
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
         System.out.println("收到消息"+s);
     }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("连接出错");
+        super.exceptionCaught(ctx, cause);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("超时退出(服务端关闭了连接)");
+        super.channelInactive(ctx);
+    }
 }
